474516d70498216fc6b8f79aee0c0eaef095a7fc
禺狨 <18119604035@163.com>
2022-12-13 11:12:00 +0800

    学习页面直播跳转排课页面

--- src/views/study/components/live-item.vue
+++ src/views/study/components/live-item.vue
@@ -97,7 +97,7 @@ export default {
         name: "liveDetail",
         query: {
           id: id,
-          tab: this.currenStatus === 2 || 1 ? 3 : 2,
+          tab: this.currenStatus !== 3 ? 3 : 2,
         },
       });
     },
