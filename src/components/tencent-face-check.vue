d27d29b6280cfba31f2889ec996862968ecae353
禺狨 <18119604035@163.com>
2023-02-07 11:03:14 +0800

    腾讯云实名认证

--- src/components/tencent-face-check.vue
+++ src/components/tencent-face-check.vue
@@ -3,38 +3,30 @@
     <div class="mask" v-if="status">
       <div class="dialog-login-box">
         <div class="tabs">
-          <div class="tit">微信扫描行下方二维码实名认证</div>
-        </div>
-        <div class="tabs">
-          <div class="time">
-            <count-down
-              v-if="wxCountDownStatus"
-              type="face"
-              :timestamp="600"
-            ></count-down>
-          </div>
+          <img
+            class="btn-close"
+            @click="cancel()"
+            src="../assets/img/commen/icon-close.png"
+          />
         </div>
         <div class="box">
           <div id="qrcode" class="qrode"></div>
         </div>
+        <p>请使用手机扫描</p>
+        <p>上方二维码完成实名认证</p>
       </div>
     </div>
   </div>
 </template>
 <script>
-import CountDown from "./count-down.vue";
 import QRCode from "qrcodejs2";
 
 export default {
   props: ["status"],
-  components: {
-    CountDown,
-  },
   data() {
     return {
       timer: null,
       loading: false,
-      wxCountDownStatus: false,
       verifyLoading: false,
       ruleId: null,
       bizToken: null,
@@ -48,9 +40,7 @@ export default {
     },
   },
   mounted() {},
-  beforeDestroy() {
-    clearInterval(this.timer);
-  },
+  beforeDestroy() {},
   methods: {
     goFaceVerify() {
       if (this.verifyLoading) {
@@ -64,27 +54,27 @@ export default {
         s_url: "PC",
       })
         .then((res) => {
-          this.wxCountDownStatus = false;
           this.verifyLoading = false;
           let link = res.data.url;
           this.ruleId = res.data.rule_id;
           this.bizToken = res.data.biz_token;
           var qrcode = new QRCode("qrcode", {
             text: link, //表示内容，可以是地址或者是文字'55566'或者参数
-            width: 300,
-            height: 300,
+            width: 250,
+            height: 250,
             colorDark: "#000000", //前景色
             colorLight: "#ffffff", //背景色
             correctLevel: QRCode.CorrectLevel.H, //容错级别
           });
-          this.wxCountDownStatus = true;
-          this.timer = setInterval(this.checkFaceVerify, 2500);
         })
         .catch((e) => {
           this.verifyLoading = false;
           this.$emit("cancel");
           this.$message.error(e.message || "无法发起实人认证");
         });
+    },
+    cancel() {
+      this.$emit("cancel");
     },
     checkFaceVerify() {
       this.$api.Member.TecentFaceVerifyQuery({
@@ -93,7 +83,7 @@ export default {
       }).then((res) => {
         if (res.data.status === 9) {
           this.$message.success("实人认证成功");
-          clearInterval(this.timer);
+
           this.$emit("change");
         }
       });
@@ -118,7 +108,7 @@ export default {
   z-index: 1001;
   .dialog-login-box {
     width: 500px;
-    max-height: 620px;
+    height: 500px;
     background: #ffffff;
     border-radius: 8px;
     display: flex;
@@ -133,35 +123,6 @@ export default {
       flex-direction: row;
       position: relative;
 
-      .linkTab {
-        position: absolute;
-        top: 5px;
-        right: 54px;
-        height: 14px;
-        font-size: 14px;
-        font-weight: 400;
-        color: #3ca7fa;
-        line-height: 14px;
-        cursor: pointer;
-        &:hover {
-          opacity: 0.8;
-        }
-      }
-      .linkTab2 {
-        position: absolute;
-        top: 5px;
-        right: 0px;
-        height: 14px;
-        font-size: 14px;
-        font-weight: 400;
-        color: #3ca7fa;
-        line-height: 14px;
-        cursor: pointer;
-        &:hover {
-          opacity: 0.8;
-        }
-      }
-
       .btn-close {
         width: 24px;
         height: 24px;
@@ -175,48 +136,6 @@ export default {
           animation: rotate360 1s;
         }
       }
-      .tit {
-        width: 100%;
-        height: 20px;
-        font-size: 20px;
-        font-weight: 500;
-        color: #333333;
-        line-height: 20px;
-        position: relative;
-        text-align: center;
-      }
-      .time {
-        width: 100%;
-        height: 20px;
-        font-size: 20px;
-        font-weight: bold;
-        color: #333333;
-        line-height: 20px;
-        position: relative;
-        text-align: center;
-      }
-      .item-tab {
-        width: auto;
-        height: 20px;
-        font-size: 20px;
-        font-weight: 500;
-        color: #666666;
-        line-height: 20px;
-        margin-right: 50px;
-        cursor: pointer;
-        position: relative;
-        &.active {
-          color: #333333;
-        }
-        .actline {
-          width: 80px;
-          height: 4px;
-          background: #3ca7fa;
-          position: absolute;
-          left: 0px;
-          top: 40px;
-        }
-      }
     }
     .box {
       width: 100%;
@@ -224,11 +143,19 @@ export default {
       display: flex;
       flex-direction: column;
       .qrode {
-        width: 300px;
-        height: 300px;
+        width: 250px;
+        height: 250px;
         margin: 0 auto;
         margin-bottom: 30px;
       }
+    }
+    p {
+      width: 100%;
+      text-align: center;
+      font-size: 16px;
+      font-weight: 400;
+      color: #333333;
+      line-height: 30px;
     }
   }
 }
