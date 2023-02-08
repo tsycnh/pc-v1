2cc9fab4e8a59e248fc15dc3ca9628625533116c
禺狨 <18119604035@163.com>
2023-02-08 09:51:43 +0800

    实名认证弹窗逻辑优化

--- src/components/tencent-face-check.vue
+++ src/components/tencent-face-check.vue
@@ -14,8 +14,7 @@
         <div class="box">
           <div id="qrcode" class="qrode"></div>
         </div>
-        <p>请使用微信扫描</p>
-        <p>上方二维码完成实名认证</p>
+        <p>学习前请用微信扫码完成实名认证</p>
       </div>
     </div>
   </div>
